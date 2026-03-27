f8df99fc0614568cac46d780fb4ff02659f2dbff
Jeff Sandquist <jeffsandservices@outlook.com>
2026-03-27 06:44:48 -0700

    Add c64 formula

---
+++ Formula/c64.rb
@@ -0,0 +1,19 @@
+class C64 < Formula
+  desc "Command your Commodore C64 Ultimate from the terminal"
+  homepage "https://github.com/jeffsand/c64"
+  url "https://registry.npmjs.org/@jeffsand/c64/-/c64-0.1.2.tgz"
+  sha256 :no_check
+  license "MIT"
+
+  depends_on "node@20"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/c64 --version").strip
+    assert_match "info", shell_output("#{bin}/c64 --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-tools
+
+Homebrew formulae for Jeff Sandquist's tools.
+
+## Install
+
+```bash
+brew tap jeffsand/tools
+brew install c64
+```
+
+## Available formulae
+
+| Formula | Description |
+|---------|-------------|
+| [c64](https://github.com/jeffsand/c64) | Command your Commodore C64 Ultimate from the terminal |
